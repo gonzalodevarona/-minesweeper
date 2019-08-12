17d1c550e8e73fcbd0c54b979de0d6ae481c63a9
Gonzalo De Varona <gavo2403@hotmail.com>
2019-08-12 14:42:52 -0500

    API done, Documents are ready, code may need an ajustment on the tests, regardless of it, everything is done

--- src/modelo/Buscaminas.java
+++ src/modelo/Buscaminas.java
@@ -351,7 +351,13 @@ public class Buscaminas {
 		return minas;
 	}
 	
-	
+	/**
+	 * Metodo que permite contar la cantidad de minas que tiene alrededor una casilla la cual limiteDeLasFilas>i>0 y limiteDeLasColumnas>j>0
+	 * @param i - La fila de la matriz
+	 * @param j - la columna de la matriz
+	 * @return int - La cantidad de minas que tiene alrededor la casilla [i][j]
+	 */
+
 	public int minasFueraDeMarco(int i, int j) {
 		int minas = 0;
 		
